97e895d5acf163c33c343b5916bdbcd2bb6ba1c6
Deepa Vasanthkumar <deepa.account@gmail.com>
2026-01-20 12:55:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-
 from dash import Dash
+from flask import Flask
 from auth.authorization import authorize
 from ui.layout import build_layout
 
@@ -7,11 +7,12 @@ app = Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
 
 @server.before_request
-def secure():
+def enforce_auth():
     authorize()
 
 app.layout = build_layout()
 
+# register callbacks
 import ui.callbacks.iceberg_callbacks
 import ui.callbacks.postgres_callbacks
 import ui.callbacks.analytics_callbacks
